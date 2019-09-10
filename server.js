6779c86100e985616f91dd0bdda1d4e2fe6f558b
Alvee Akand <AlveeM@users.noreply.github.com>
2019-09-10 12:56:52 +0600

    updated nodemon

--- server.js
+++ server.js
@@ -24,16 +24,14 @@ db.select('*').from('users').then(data => {
 })
 
 const app = express();
-app.use(bodyParser.json());
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin',  signin.handleSignin(db, bcrypt) )
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
